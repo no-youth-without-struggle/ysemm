8c2863122ae94d71b93311a1c8c736fd7eafc7b8
bai <252027426@qq.com>
2019-07-27 13:17:38 +0800

    首次提交

---
+++ src/main/java/com/aaa/ysemm/manage/App.java
@@ -0,0 +1,12 @@
+package com.aaa.ysemm.manage;
+
+/**
+ * @ProjectName: ysemm
+ * @Package: com.aaa.ysemm.manage
+ * @Author: ${白帅}
+ * @Description: ${description}
+ * @Date: 2019/7/27 13:16
+ * @Version: 1.0
+ */
+public class App {
+}
