cc582e96e97a15bd29958f2c92bffad1eb4bfc2c
bai <252027426@qq.com>
2019-07-28 14:59:27 +0800

    保存提交

--- src/main/java/com/aaa/ysemm/DemoApplication.java
+++ src/main/java/com/aaa/ysemm/DemoApplication.java
@@ -3,7 +3,6 @@ package com.aaa.ysemm;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 @MapperScan({"com.aaa.ysemm.customer.dao","com.aaa.ysemm.manage.dao"})
 public class DemoApplication {
--- src/main/java/com/aaa/ysemm/customer/controller/EmpController.java
+++ src/main/java/com/aaa/ysemm/customer/controller/EmpController.java
@@ -1,13 +1,10 @@
 package com.aaa.ysemm.customer.controller;
-
 import com.aaa.ysemm.customer.entity.Emp;
 import com.aaa.ysemm.customer.entity.Page;
 import com.aaa.ysemm.customer.service.EmpService;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
