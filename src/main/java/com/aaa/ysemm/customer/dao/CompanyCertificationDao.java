c5711c0811664bc40fe47089a1820af6ae496687
tom_glb <good@chen.com>
2019-08-05 10:15:25 +0800

    认证完成1.0v

--- src/main/java/com/aaa/ysemm/customer/dao/CompanyCertificationDao.java
+++ src/main/java/com/aaa/ysemm/customer/dao/CompanyCertificationDao.java
@@ -1,6 +1,7 @@
 package com.aaa.ysemm.customer.dao;
 
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Update;
 import org.springframework.stereotype.Repository;
 
 import java.util.Map;
@@ -13,4 +14,14 @@ public interface CompanyCertificationDao {
             "(#{uid},#{cname},#{email},#{telephone}," +
             "#{phone},#{nature},#{address},#{bankCard})")
     void adduser(Map map);
+
+    @Update("update company set reverse_IDCard=#{s} where uid=#{uid}")
+    void uploadanouther(String s, Integer uid);
+
+    @Update("update company set business_license=#{s} where uid=#{uid}")
+    void businesslicense(String s, Integer uid);
+
+    @Update("update company set pname=#{name},IDCard=#{code},sex=#{sex}" +
+            " where uid=#{uid}")
+    void upload(Map map1);
 }
